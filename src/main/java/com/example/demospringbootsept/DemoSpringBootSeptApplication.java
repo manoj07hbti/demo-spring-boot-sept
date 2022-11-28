c4e592e692d66cb0823b26ca2c48e723ed4079ab
60manojkumar@gmail.com <Motivated@2019>
2022-11-28 19:37:32 +0530

    Initial commit

---
+++ src/main/java/com/example/demospringbootsept/DemoSpringBootSeptApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringbootsept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootSeptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootSeptApplication.class, args);
+	}
+
+}
