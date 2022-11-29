12360981c1c5fce039d6ebf31549bccb5021af2a
60manojkumar@gmail.com <Motivated@2019>
2022-11-29 19:46:28 +0530

    Controller Day1

---
+++ src/main/java/com/example/demospringbootsept/controller/DemoController.java
@@ -0,0 +1,31 @@
+package com.example.demospringbootsept.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+
+    @RequestMapping("/message")
+    public void hello(){
+
+        System.out.println("Hello World...");
+    }
+
+
+    @RequestMapping("/welcome")
+    public String welcome(){
+
+        String msg="Welcome to Spring boot ....";
+
+        return msg;
+    }
+
+    @RequestMapping("/make_square")
+    public int makeSquare(){
+
+        return 3*3;
+    }
+
+}
