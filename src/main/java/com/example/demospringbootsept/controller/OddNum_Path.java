48c72b6ef3ade02984f8c33c0bf209f24038c5f0
1994Khushi <ayangelyoung@gmai.com>
2022-12-02 17:38:05 +0530

    Odd path variable(Second method)

---
+++ src/main/java/com/example/demospringbootsept/controller/OddNum_Path.java
@@ -0,0 +1,19 @@
+package com.example.demospringbootsept.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OddNum_Path {
+    @RequestMapping("/odd/{n}/{m}")
+    public String odd(@PathVariable int n,@PathVariable int m) {
+        String result = "print odd number :-";
+        for (int i = 1; i <= m; i++) {
+            if (i % 2 != 0) {
+                result = result + "," + i;
+            }
+        }
+        return result;
+    }
+}
