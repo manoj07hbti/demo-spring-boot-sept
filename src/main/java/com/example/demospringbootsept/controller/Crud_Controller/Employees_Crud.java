20bbb73c6ddb4d34e92771bc3cb973642d04cf25
1994Khushi <ayangelyoung@gmai.com>
2022-12-06 19:07:36 +0530

    requestbody

---
+++ src/main/java/com/example/demospringbootsept/controller/Crud_Controller/Employees_Crud.java
@@ -0,0 +1,25 @@
+package com.example.demospringbootsept.controller.Crud_Controller;
+
+import com.example.demospringbootsept.model.Employee;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class Employees_Crud {
+    //CRUD
+    //TODO C: create/insert
+    ArrayList<Employee> employeeArrayList = new ArrayList<>();
+
+    @RequestMapping("/add_emp")
+    public Employee addEmployee( @RequestBody Employee info){
+        employeeArrayList.add(info);
+        return info;
+    }
+    @RequestMapping("/get_emp")
+    public ArrayList<Employee> getEmployeeArrayList(){
+        return employeeArrayList;
+    }
+}
