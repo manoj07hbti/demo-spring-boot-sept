243347af393081094e10e7ed221eee0fad5593c0
nikhlesh1206 <sbieber278@gmail.com>
2022-12-06 22:26:21 +0530

    CRUD complete

--- src/main/java/com/example/demospringbootsept/controller/EmployeeCRUDController.java
+++ src/main/java/com/example/demospringbootsept/controller/EmployeeCRUDController.java
@@ -1,25 +1,42 @@
 package com.example.demospringbootsept.controller;
-
 import com.example.demospringbootsept.model.Employee;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
+@RestController
 public class EmployeeCRUDController {
 
     ArrayList <Employee> employeeArrayList=new ArrayList<>();
     //CRUD
     //1.C : CREATE /INSERT  ---> POST
 
-    @RequestMapping("/add_employee")
+    @RequestMapping(value="/add_employee", method= RequestMethod.POST)
     public Employee addEmployee(@RequestBody  Employee employee){
         employeeArrayList.add(employee);
         return employee;
     }
 
-    @RequestMapping("/get_employee")
+    @RequestMapping(value="/get_employee", method=RequestMethod.GET)
     public ArrayList<Employee> getData(){
+
         return employeeArrayList;
+    }
+
+    @RequestMapping(value="/update_employee/{index}/{name}", method=RequestMethod.PUT)
+    public String updateEmployee(@PathVariable int index,@PathVariable String name){
+
+        Employee employee=employeeArrayList.get(index);
+        employee.setName(name);
+
+        return "Name Updated successfully..";
+    }
+
+    @RequestMapping(value="/remove_employee", method = RequestMethod.DELETE)
+    public String removeEmployee(@RequestParam int index){
+
+        employeeArrayList.remove(index);
+
+        return "Employee deleted Successfully";
     }
 }
