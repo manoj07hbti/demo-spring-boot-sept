71d4c2be239c1b8f1d58ff7e223cd69e099e619b
1994Khushi <ayangelyoung@gmai.com>
2022-12-07 20:22:34 +0530

    Request body

--- src/main/java/com/example/demospringbootsept/controller/crud_Controller/Employees_Crud.java
+++ src/main/java/com/example/demospringbootsept/controller/crud_Controller/Employees_Crud.java
@@ -1,9 +1,7 @@
 package com.example.demospringbootsept.controller.crud_Controller;
 
 import com.example.demospringbootsept.model.Employee;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -13,13 +11,30 @@ public class Employees_Crud {
     //TODO C: create/insert
     ArrayList<Employee> employeeArrayList = new ArrayList<>();
 
-    @RequestMapping("/add_emp")
-    public Employee addEmployee( @RequestBody Employee info){
+    @RequestMapping(value = "/add_emp", method = RequestMethod.POST)
+    public Employee addEmployee(@RequestBody Employee info) {
         employeeArrayList.add(info);
         return info;
     }
-    @RequestMapping("/get_emp")
-    public ArrayList<Employee> getEmployeeArrayList(){
+
+    @RequestMapping(value = "/get_emp", method = RequestMethod.GET)
+    public ArrayList<Employee> getEmployeeArrayList() {
         return employeeArrayList;
+    }
+
+    //todo update crud method
+    @RequestMapping(value = "/update_name/{index}/{name}" ,method = RequestMethod.PUT)
+    public String updateEmployees(@PathVariable int index, @PathVariable String name) {
+        Employee emp = employeeArrayList.get(index);
+        emp.setName(name);
+        return "change name";
+    }
+
+    //todo remove  crud method
+
+    @RequestMapping(value = "/remove" , method = RequestMethod.DELETE)
+    public String removeEmployees(@RequestParam int index) {
+        employeeArrayList.remove(index);
+        return "remove successful";
     }
 }
