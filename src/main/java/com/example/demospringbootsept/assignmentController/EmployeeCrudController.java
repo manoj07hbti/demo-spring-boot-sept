11d408819ae7c23ba4cb71d0adbff3ceec0fddae
ankitchauhan26 <68079328+ankitchauhan26@users.noreply.github.com>
2022-12-08 18:49:56 +0530

    Day5_CRUD Operation

--- src/main/java/com/example/demospringbootsept/assignmentController/EmployeeCrudController.java
+++ src/main/java/com/example/demospringbootsept/assignmentController/EmployeeCrudController.java
@@ -2,27 +2,46 @@ package com.example.demospringbootsept.assignmentController;
 
 
 import com.example.demospringbootsept.model.Employee;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 @RestController
 public class EmployeeCrudController {
+    ArrayList <Employee> employeeArrayList = new ArrayList<>();
 
 
-    ArrayList<Employee> employeeArrayList = new ArrayList<>();
+//    CRUD Operation
+//    1.->  C - Create  - Post
 
-@RequestMapping("/add_employee")
+    @RequestMapping(value = "/add_employee", method = RequestMethod.POST)
     public Employee addEmployee(@RequestBody Employee employee){
+
         employeeArrayList.add(employee);
+
         return employee;
     }
-
-    @RequestMapping("/get_employee")
+//     2. -> R  --  Read --------Get
+    @RequestMapping(value = "/get_employee" , method = RequestMethod.GET)
     public  ArrayList<Employee> getData(){
         return employeeArrayList;
     }
 
+    //     3. -> U  --  Update ------------- PUT
+
+    @RequestMapping(value = "/update_employee/{index}/{name}" ,method = RequestMethod.DELETE )
+    public String updateEmployee(@PathVariable int index, @PathVariable String name){
+        Employee employee=employeeArrayList.get(index);
+        employee.setEmpName(name);
+
+        return "Name Update Successfully..";
+    }
+    //     3. -> D  --  Delete ------------- Del
+    @RequestMapping(value = "/remove_employee", method = RequestMethod.DELETE )
+    public String removeEmployee(@RequestParam int index){
+        employeeArrayList.remove(index);
+
+        return "Employee DeletedSuccessfully";
+
+    }
 }
