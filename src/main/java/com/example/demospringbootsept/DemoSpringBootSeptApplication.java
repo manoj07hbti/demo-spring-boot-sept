c24d508097ac8f404c3ee0e360d48fc2a630f24d
1994Khushi <ayangelyoung@gmai.com>
2022-12-18 15:55:21 +0530

    properties and dependency

--- src/main/java/com/example/demospringbootsept/DemoSpringBootSeptApplication.java
+++ src/main/java/com/example/demospringbootsept/DemoSpringBootSeptApplication.java
@@ -3,10 +3,14 @@ package com.example.demospringbootsept;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class DemoSpringBootSeptApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Setting the timezone"+ TimeZone.getTimeZone("GMT+9:00").getID());
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT+9:00"));
 		SpringApplication.run(DemoSpringBootSeptApplication.class, args);
 	}
 
